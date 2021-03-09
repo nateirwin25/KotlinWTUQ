070490408b4a65d35dec31f6f1e9f5020cf7e1be
nhans32 <56236772+nhans32@users.noreply.github.com>
2021-03-09 00:24:31 -0800

    skeleton of assign 5

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,26 @@
+
+open class Value()
+class NumV(var n: Int) : Value()
+class BoolV(var bool: Boolean) : Value()
+class StringV(var str: String) : Value()
+class CloV(var parms: List<String>, var body: Value) : Value()
+class PrimV(var oper: Char, var operands: List<ExprC>)
+
+class Binding(var name: Char, var v: Value)
+class Env(var bindings: List<Binding>)
+
+open class ExprC()
+class NumC(var n: Int) : ExprC()
+class StringC(var str: String) : ExprC()
+class ifC(var x: ExprC, var th: ExprC, var el: ExprC) : ExprC()
+class IdC(var x: Char) : ExprC()
+class AppC(var func: ExprC, var args: List<ExprC>) : ExprC()
+class LamC(var args: List<Char>, var body: ExprC) : ExprC()
+
+fun interp(expr: ExprC, env: Env) {
+    println(expr)
+}
+
 fun main() {
-    print("What's you're name?: ")
-    val name = readLine()
-    println("Hello $name!")
+    println("Hello")
 }
